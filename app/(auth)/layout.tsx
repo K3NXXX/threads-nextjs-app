c35f27a6d182cf0ed1e1e23b34b21a784f31bf5f
Volodya Andrushevskyi <xxvolodyax7@gmail.com>
2024-03-18 20:17:17 +0200

    header: added logo and text

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,10 +2,7 @@ import { ClerkProvider } from "@clerk/nextjs"
 import { Inter } from "next/font/google"
 import '../globals.css'
 
-export const metadata = {
-    title: 'Threads',
-    description: "A Next.js 14 Meta Threads Application"
-}
+
 
 const inter = Inter({subsets: ['latin']})
 
