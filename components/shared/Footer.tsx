06f44258f0ebaa8a675df187edc7d14587a88446
Volodya Andrushevskyi <xxvolodyax7@gmail.com>
2024-03-19 20:06:58 +0200

    footer: added text and links

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,6 +1,31 @@
+'use client'
+import { sidebarLinks } from "@/constants"
+import Image from "next/image"
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+
 const Footer = () => {
+  const pathname = usePathname()
   return (
-    <div>Footer</div>
+    <section className="bottombar">
+      <div className="bottombar_container">
+      {sidebarLinks.map((link) => {
+          const isActive = pathname === link.route
+          return (
+          <Link href={link.route} 
+                key={link.label}
+                className={`leftsidebar_link ${isActive ? 'bg-primary-500' : ''}`}>
+                <Image 
+                  src={link.imgURL}
+                  alt={link.label}
+                  width={24}
+                  height={24}
+                />
+                <p className="text-light-1 max-lg:hidden">{link.label}</p>
+          </Link>
+        )})}
+      </div>
+    </section>
   )
 }
 
